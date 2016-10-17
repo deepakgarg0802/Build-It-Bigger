730b9bbc16bee1a2720b4a8d83e906d7e45a4961
deepakgarg0802 <deepakgarg0802@gmail.com>
2016-10-17 18:34:33 +0530

    Add : Android Library for viewing joke (Step 2)
    * Android library is made which provides an activity to view joke

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 import com.example.NerdyJoke;
+import com.example.deepakgarg.myjokedisplaylibrary.ViewJokeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,7 +47,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void tellJoke(View view) {
 
-        Toast.makeText(this, random_joke.getJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent= new Intent(getApplication(), ViewJokeActivity.class);
+        intent.putExtra("joke",random_joke.getJoke());
+        startActivity(intent);
+        //Toast.makeText(this, random_joke.getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ myjokedisplaylibrary/src/main/java/com/example/deepakgarg/myjokedisplaylibrary/ViewJokeActivity.java
@@ -0,0 +1,24 @@
+package com.example.deepakgarg.myjokedisplaylibrary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ViewJokeActivity extends AppCompatActivity {
+    public static final String INTENT_LABEL="joke";
+    String joke;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_joke);
+
+        textView=(TextView)findViewById(R.id.textView);
+        joke= getIntent().getStringExtra(INTENT_LABEL);
+
+        textView.setText(joke);
+        Toast.makeText(getApplicationContext(),"Lol !!!!!!!!!",Toast.LENGTH_SHORT).show();
+    }
+}
